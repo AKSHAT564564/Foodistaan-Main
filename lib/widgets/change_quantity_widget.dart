eca4416cfa30804d16d0a658b44970194db06ba8
Eishu Pal <eishu18sept@gmail.com>
2022-02-20 11:52:24 +0530

    changes

--- lib/widgets/change_quantity_widget.dart
+++ lib/widgets/change_quantity_widget.dart
@@ -17,40 +17,43 @@ class _ChangeQuantityWidgetState extends State<ChangeQuantityWidget> {
       alignment: Alignment.center,
       height: MediaQuery.of(context).size.height * 0.04,
       // width: MediaQuery.of(context).size.height * 0.04,
-      child: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-        Spacer(),
-        InkWell(
-          onTap: () async {
-            await CartFunctions().increaseQuantity(widget.cartId,
-                widget.data['id'], widget.data['quantity'], false);
-          },
-          child: Icon(
-            widget.data['quantity'].toString() == '1'
-                ? FontAwesomeIcons.trashAlt
-                : FontAwesomeIcons.minusCircle,
-            color: Colors.amber,
-            size: 18,
-          ),
-        ),
-        SizedBox(
-          width: 6,
-        ),
-        Text(widget.data['quantity']),
-        SizedBox(
-          width: 6,
-        ),
-        InkWell(
-          onTap: () async {
-            await CartFunctions().increaseQuantity(widget.cartId,
-                widget.data['id'], widget.data['quantity'], true);
-          },
-          child: Icon(
-            FontAwesomeIcons.plusCircle,
-            color: Colors.amber,
-            size: 18,
-          ),
-        ),
-      ]),
+      child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Spacer(),
+            InkWell(
+              onTap: () async {
+                await CartFunctions().increaseQuantity(widget.cartId,
+                    widget.data['id'], widget.data['quantity'], false);
+              },
+              child: Icon(
+                widget.data['quantity'].toString() == '1'
+                    ? FontAwesomeIcons.trashAlt
+                    : FontAwesomeIcons.minusCircle,
+                color: Colors.amber,
+                size: 18,
+              ),
+            ),
+            SizedBox(
+              width: 6,
+            ),
+            Text(widget.data['quantity']),
+            SizedBox(
+              width: 6,
+            ),
+            InkWell(
+              onTap: () async {
+                await CartFunctions().increaseQuantity(widget.cartId,
+                    widget.data['id'], widget.data['quantity'], true);
+              },
+              child: Icon(
+                FontAwesomeIcons.plusCircle,
+                color: Colors.amber,
+                size: 18,
+              ),
+            ),
+          ]),
     );
   }
 }
