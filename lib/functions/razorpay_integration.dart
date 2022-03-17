5139b8eded5915e93430ac7bdfede19ca2236e57
RAJAN PRATAP SINGH <53988654+LONEWOLF-tech@users.noreply.github.com>
2022-03-17 15:23:11 +0530

    White Screen fix Of Payment

--- lib/functions/razorpay_integration.dart
+++ lib/functions/razorpay_integration.dart
@@ -3,6 +3,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:foodistan/constants.dart';
+import 'package:foodistan/customLoadingSpinner.dart';
 import 'package:sizer/sizer.dart';
 import 'package:foodistan/MainScreenFolder/mainScreenFile.dart';
 import 'package:foodistan/cart_screens/login_pay_cart_screen_main.dart';
@@ -21,15 +22,14 @@ class RazorPayScreen extends StatefulWidget {
   Map<String, dynamic> items;
   Map<String, dynamic> deliveryAddress;
   String orderType;
-  RazorPayScreen({
-    required this.finalPrice,
-    required this.items,
-    required this.cartId,
-    required this.vednorId,
-    required this.vendorName,
-    required this.deliveryAddress,
-    required this.orderType
-  });
+  RazorPayScreen(
+      {required this.finalPrice,
+      required this.items,
+      required this.cartId,
+      required this.vednorId,
+      required this.vendorName,
+      required this.deliveryAddress,
+      required this.orderType});
 
   @override
   _RazorPayScreenState createState() => _RazorPayScreenState();
@@ -82,8 +82,16 @@ class _RazorPayScreenState extends State<RazorPayScreen> {
   void _handlePaymentSuccess(PaymentSuccessResponse response) {
     String paymentId = response.paymentId!;
     OrderFunction()
-        .placeOrder(widget.vednorId, widget.vendorName, userNumber, itemMap,
-            widget.finalPrice, paymentId, widget.cartId, widget.deliveryAddress,widget.orderType)
+        .placeOrder(
+            widget.vednorId,
+            widget.vendorName,
+            userNumber,
+            itemMap,
+            widget.finalPrice,
+            paymentId,
+            widget.cartId,
+            widget.deliveryAddress,
+            widget.orderType)
         .then((value) {
       Navigator.push(
           context,
@@ -160,7 +168,92 @@ class _RazorPayScreenState extends State<RazorPayScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(),
+      body: Container(
+        // height: 100.h,
+        // width: double.infinity,
+        color: Colors.white,
+        child: Stack(
+          // crossAxisAlignment: CrossAxisAlignment.center,
+          // mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              alignment: Alignment.center,
+              padding: const EdgeInsets.symmetric(
+                horizontal: 11,
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset(
+                    'assets/images/emojione_sad-but-relieved-face.png',
+                    height: 15.h,
+                    width: 15.h,
+                  ),
+                  SizedBox(height: 4.5.h),
+                  Text(
+                    'Opps something went wrong!',
+                    style: TextStyle(
+                      // color: Colors.white,
+                      fontWeight: FontWeight.w600,
+                      fontSize: 18.5.sp,
+                    ),
+                  ),
+                  SizedBox(height: 2.5.h),
+                  Text(
+                    'Don’t worry if something debited from your account, rest assured it’ll be back in your account within 48 hours.',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: kGreyDark2,
+                      fontSize: 10.5.sp,
+                    ),
+                  ),
+                  // CustomLoadingSpinner()
+                ],
+              ),
+            ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: GestureDetector(
+                onTap: () async {
+                  Navigator.pushAndRemoveUntil(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => MainScreen(
+                                currentIndex: 1,
+                              )),
+                      (route) => false);
+                },
+                child: Container(
+                  height: 45,
+                  margin: EdgeInsets.only(left: 10, right: 10, bottom: 8.5.h),
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    // color: kRedPure,
+                    color: kYellowL,
+                    border: Border.all(
+                      // color: kRedPure,
+                      color: kYellowL,
+                      width: 1.5,
+                    ),
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  child: Center(
+                    child: Text(
+                      'Go Back To Cart',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.w600,
+                        fontSize: 15.sp,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
